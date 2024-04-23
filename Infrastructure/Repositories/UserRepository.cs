b043e248681a68d53913a58d01a320f515986b39
Sakretsu <saul.pr88@gmail.com>
2024-04-22 18:26:32 -0700

    first commit

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿// using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.Types;
+using System.Collections.Generic;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString = "server=localhost;database=Empresa;user=sa;password=Passw0rd";
+
+        public IEnumerable<Users> GetAllUsers()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM Users";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return new Users
+                            {
+                                Id = Convert.ToInt32(reader["id"]),
+                                Name = reader["name"].ToString(),
+                                Email = reader["email"].ToString(),
+                                Password = reader["password"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+        }
+
+        // ... (implementar otros métodos de acceso a datos)
+    }
+
+    public class Users {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ src/Users.Api/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace Users.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers() {
+            
+        UserRepository userRepository = new UserRepository();
+            var personas = userRepository.GetAllUsers();
+            return Ok(personas);
+
+
+        }
+
+
+        //public IActionResult GetUserss()
+        //{
+        //    IActionResult actionResult = null;
+        //}
+    }
+}
